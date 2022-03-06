c2190abf7dd857cb8a14aab1f9ca6c5904665650
arduino90120 <60314719+arduino90120@users.noreply.github.com>
2022-03-06 12:37:45 +0100

    Add files via upload

---
+++ minecraft_control.sh
@@ -0,0 +1,13 @@
+#sudo apt update 
+
+sudo apt install -y python3-smbus
+
+sudo apt install minecraft_pi
+
+git clone https://github.com/ric96/motioncraft
+
+pip3 install mcpi
+
+cd motioncraft
+
+python3 motioncraft.py
